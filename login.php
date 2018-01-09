3194356aba148f9c9f096ec5736bb1c581ff0770
高佐遥平 <yohei.030516@gmail.com>
2018-01-09 15:48:53 +0800

    自動ログイン完成

--- login.php
+++ login.php
@@ -1,7 +1,19 @@
 <?php
+session_start();
 
-  // DBに接続
-  require('dbconnect.php');
+// クッキー情報が存在してたら（自動ログイン）
+// $_POSTにログイン情報を保存します
+if (isset($_COOKIE["email"]) && !empty($_COOKIE["email"])){
+  $_POST["email"] = $_COOKIE["email"];
+  $_POST["password"] = $_COOKIE["password"];
+  // クッキー情報が入っている=自動ログインがonになっている
+  $_POST["save"] = "on";
+
+}
+
+
+// DBに接続
+require('dbconnect.php');
 
   // POST送信されたいたら
   if (isset($_POST) && !empty($_POST)){
@@ -13,7 +25,7 @@
 
       // SQL文実行
       // パスワードは、入力されたものを暗号化した上で使用する
-      $data = array($_POST['email'],sha1($_POST['password']));
+      $data = array($_POST["email"],sha1($_POST["password"]));
       $stmt = $dbh->prepare($sql);
       $stmt->execute($data);
 
@@ -24,6 +36,30 @@
       // echo "<pre>";
       // var_dump($member);
       // echo "</pre>";
+
+      if ($member == false){
+        // 認証失敗
+        $error["login"] = "failed";
+      }else{
+        // 認証成功
+        // 1セッション変数に、会員のidを保存
+        $_SESSION["id"] = $member["member_id"];
+
+        // 2.ログインした時間をセッション変数に保存
+        $_SESSION["time"] = time();
+
+        // 3.自動ログインの処理
+        if ($_POST["save"] == "on"){
+          // クッキーにログイン情報を記録
+          // setcookie(保存したい名前,保存したい値,保存したい期間：秒数)
+          setcookie('email',$_POST["email"], time()+60*60*24*14);
+          setcookie('password',$_POST["password"], time()+60*60*24*14);
+        }
+
+        // 4.ログイン後の画面に移動
+        header("Location: index.php");
+        exit();
+      }
 
     } catch (Exception $e) {
       
@@ -92,6 +128,16 @@
               <input type="password" name="password" class="form-control" placeholder="">
             </div>
           </div>
+          <!-- 自動ログイン -->
+          <div class="form-group">
+            <label class="col-sm-4 control-label">自動ログイン</label>
+            <div class="col-sm-8">
+              <input type="checkbox" name="save">オンにする
+            </div>
+          </div>
+          <?php if ((isset($error["login"])) && ($error["login"] == "failed")) { ?>
+              <p class="error">* emailかパスワードが間違っています</p>
+              <?php } ?>
           <input type="submit" class="btn btn-default" value="ログイン">
         </form>
       </div>
